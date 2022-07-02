a88d5c9ff91797a56894be20cb0ac9370e354863
Alexei Alayo <alexei.alayo@sivah.net>
2022-07-02 12:08:29 -0400

    Some change

--- herokubot.py
+++ herokubot.py
@@ -131,7 +131,7 @@ def ld(update, context):
 
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Sorry, I didn't understand that command.")
+                             text="Sorry, I didn't understand this command.")
 
 
 def help_command(update, context):
